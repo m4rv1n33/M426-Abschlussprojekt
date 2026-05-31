140c9602797c456ac865db0d531c08d2e22f3847
Elysiummmm <57315331+Elysiummmm@users.noreply.github.com>
2026-05-31 23:28:25 +0200

    render shape as point cloud

--- src/main/java/nusextended/m426/HelloController.java
+++ src/main/java/nusextended/m426/HelloController.java
@@ -13,6 +13,14 @@ import nusextended.m426.game.NumberFormatter;
 import nusextended.m426.model.Shape;
 
 public class HelloController {
+    private final Point2D shapeOrigin = new Point2D(407.5, 250);
+    private final double shapeRadius = 225;
+    private final double vertexSize = 16;
+
+    private double lastFrame;
+    private double delta;
+    private double time = 0;
+
     private GameState gameState;
     private GraphicsContext shapeG2D;
     private GraphicsContext upgradesG2D;
@@ -39,6 +47,8 @@ public class HelloController {
     protected void initialize() {
         shapeG2D = shapeCanvas.getGraphicsContext2D();
         upgradesG2D = upgradesCanvas.getGraphicsContext2D();
+
+        lastFrame = System.currentTimeMillis();
     }
 
     public void setGameState(GameState gameState) {
@@ -46,6 +56,9 @@ public class HelloController {
     }
 
     public void updateCurrencyDisplay(double currency, Shape shape, int prestigeLevel) {
+        delta = System.currentTimeMillis() - lastFrame;
+        time += delta;
+
         currencyDisplay.setText(NumberFormatter.formatCurrency(currency));
 
         if (prestigeLevel < 1) {
@@ -60,10 +73,27 @@ public class HelloController {
         shapeG2D.fillRect(0, 0, shapeCanvas.getWidth(), shapeCanvas.getHeight());
 
         int vertexCount = gameState.getActiveShape().getVertices();
+        double anglePerVertex = Math.PI * 2f / vertexCount;
         Point2D[] points = new Point2D[vertexCount];
 
-        for (int i = 0; i < vertexCount; i++) {
+        if (vertexCount > 1) {
+            for (int i = 0; i < vertexCount; i++) {
+                Point2D p = new Point2D(
+                        Math.cos(anglePerVertex * i),
+                        Math.sin(anglePerVertex * i)
+                ).multiply(shapeRadius);
 
+                points[i] = p.add(shapeOrigin);
+            }
+        } else {
+            points[0] = shapeOrigin;
+        }
+
+        shapeG2D.setFill(Paint.valueOf("white"));
+        for (Point2D p : points) {
+            shapeG2D.fillOval(
+                    p.getX() - vertexSize / 2, p.getY() - vertexSize / 2,
+                    vertexSize, vertexSize);
         }
     }
 }
