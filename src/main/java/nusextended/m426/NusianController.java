7979327cdfb05e6e2b630a7c2f132d3ea44ffc44
Elysiummmm <57315331+Elysiummmm@users.noreply.github.com>
2026-06-21 16:55:40 +0200

    i hope i can rebase now

--- src/main/java/nusextended/m426/NusianController.java
+++ src/main/java/nusextended/m426/NusianController.java
@@ -1,7 +1,10 @@
+<<<<<<<< HEAD:src/main/java/nusextended/m426/HelloController.java
+========
 package nusextended.m426;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -9,6 +12,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import nusextended.m426.game.BalanceConfig;
 import nusextended.m426.game.GameState;
 import nusextended.m426.game.NumberFormatter;
@@ -16,6 +20,7 @@ import nusextended.m426.game.PrestigeStateManager;
 import nusextended.m426.game.UpgradeNode;
 import nusextended.m426.game.UpgradeStateManager;
 import nusextended.m426.model.Shape;
+import nusextended.m426.model.UpgradeNode;
 
 
 public class NusianController {
@@ -23,16 +28,24 @@ public class NusianController {
     private final double shapeRadius = 225;
     private final double vertexSize = 16;
     private final double lineWidth = 6;
-    private final double spinSpeed = 0.1;
+    private final double spinSpeed = 0.1; // radii per second
 
     private double lastFrame;
     private double delta;
+    private double time = 0;
+
     private double spinOffset;
 
     private GameState gameState;
     private UpgradeStateManager upgradeManager;
     private GraphicsContext shapeG2D;
     private PrestigeStateManager prestigeManager;
+    private GraphicsContext upgradesG2D;
+    private Point2D upgradeTreeOffset;
+
+    private Point2D mouseDragStartPos;
+    private Point2D mouseDragStartOffset;
+    private boolean upgradeTreeBeingDragged = false;
 
     @FXML
     public Text currencyDisplay;
@@ -53,14 +66,48 @@ public class NusianController {
     public Canvas upgradesCanvas;
 
     @FXML
-    public Button prestigeButton;
+    private Button prestigeButton;
+
+    @FXML
+    private Canvas upgradesCanvas;
 
     @FXML
     protected void initialize() {
         shapeG2D = shapeCanvas.getGraphicsContext2D();
+        upgradesG2D = upgradesCanvas.getGraphicsContext2D();
+
         lastFrame = System.currentTimeMillis();
+
         shapeG2D.setLineWidth(lineWidth);
         shapeG2D.setStroke(Paint.valueOf("white"));
+
+        upgradesG2D.setStroke(Paint.valueOf("white"));
+        upgradesG2D.setTextAlign(TextAlignment.CENTER);
+        upgradesG2D.setTextBaseline(VPos.CENTER);
+
+        shapeG2D.setFill(Paint.valueOf("black"));
+        shapeG2D.fillRect(0, 0, 5, shapeCanvas.getHeight());
+
+        upgradeTreeOffset = new Point2D(
+                upgradesCanvas.getWidth() / 2, upgradesCanvas.getHeight() / 2);
+
+        upgradesCanvas.setOnMousePressed(ev -> {
+            upgradeTreeBeingDragged = true;
+            mouseDragStartPos = new Point2D(
+                    ev.getX(),
+                    ev.getY());
+            mouseDragStartOffset = upgradeTreeOffset;
+        });
+
+        upgradesCanvas.setOnMouseMoved(ev -> {
+            if (upgradeTreeBeingDragged) {
+                Point2D diff = mouseDragStartPos.subtract(
+                        ev.getX(),
+                        ev.getY());
+
+                upgradeTreeOffset = mouseDragStartOffset.subtract(diff);
+            }
+        });
     }
 
     public void setGameState(GameState gameState) {
@@ -84,7 +131,7 @@ public class NusianController {
 
     private void refreshPrestigeUpgradeButtons() {
         prestigeUpgradesContainer.getChildren().clear();
-            
+
         for (UpgradeNode node : gameState.getPrestigeTree().getNodes()) {
             Button btn = new Button();
             updatePrestigeUpgradeButton(btn, node);
@@ -109,7 +156,7 @@ public class NusianController {
         btn.setText(node.getName()
     + " (Lv. " + node.getPurchaseCount() + ")"
     + " / " + NumberFormatter.formatCurrency(cost) + " PP");
-    
+
         btn.setDisable(!canAfford);
     }
 
@@ -123,6 +170,7 @@ public class NusianController {
     public void updateCurrencyDisplay(double currency, Shape shape, int prestigeLevel, double prestigePoints) {
         delta = System.currentTimeMillis() - lastFrame;
         lastFrame = System.currentTimeMillis();
+        time += delta / 1000.0;
 
         currencyDisplay.setText(NumberFormatter.formatCurrency(currency));
 
@@ -148,10 +196,48 @@ public class NusianController {
         });
         */
 
-        shapeG2D.clearRect(0, 0, shapeCanvas.getWidth(), shapeCanvas.getHeight());
+        shapeG2D.clearRect(4, 0, shapeCanvas.getWidth(), shapeCanvas.getHeight());
         shapeG2D.setFill(Paint.valueOf("#999999"));
-        shapeG2D.fillRect(0, 0, shapeCanvas.getWidth(), shapeCanvas.getHeight());
+        shapeG2D.fillRect(4, 0, shapeCanvas.getWidth(), shapeCanvas.getHeight());
 
+        renderShape();
+        renderUpgradeTree();
+    }
+
+    private void renderUpgradeTree() {
+        upgradesG2D.clearRect(0, 0, upgradesCanvas.getWidth(), upgradesCanvas.getHeight());
+        upgradesG2D.setFill(Paint.valueOf("#999999"));
+        upgradesG2D.fillRect(0, 0, upgradesCanvas.getWidth(), upgradesCanvas.getHeight());
+
+        upgradesG2D.setFill(Paint.valueOf("#ffffff"));
+
+        // do this in two steps so the lines actually look nice
+
+        upgradesG2D.setLineWidth(1);
+        for (UpgradeNode upgrade : gameState.getUpgradeTree().getNodes()) {
+            Point2D transformed = upgrade.getLocation().add(upgradeTreeOffset);
+
+            for (UpgradeNode dep : upgrade.getPreviousNodes()) {
+                Point2D p = dep.getLocation().add(upgradeTreeOffset);
+                upgradesG2D.strokeLine(transformed.getX(), transformed.getY(), p.getX(), p.getY());
+            }
+        }
+
+        upgradesG2D.setLineWidth(3);
+        for (UpgradeNode upgrade : gameState.getUpgradeTree().getNodes()) {
+            double radius = upgrade.getVisualSize();
+            Point2D transformed = upgrade.getLocation().add(upgradeTreeOffset);
+
+            upgradesG2D.setFill(Paint.valueOf("#999999"));
+            upgradesG2D.fillOval(transformed.getX() - radius / 2, transformed.getY() - radius / 2, radius, radius);
+
+            upgradesG2D.setFill(Paint.valueOf("white"));
+            upgradesG2D.strokeOval(transformed.getX() - radius / 2, transformed.getY() - radius / 2, radius, radius);
+            upgradesG2D.fillText(upgrade.getIcon(), transformed.getX(), transformed.getY());
+        }
+    }
+
+    private void renderShape() {
         int vertexCount = gameState.getActiveShape().getVertices();
         double anglePerVertex = Math.TAU / vertexCount;
         Point2D[] points = new Point2D[vertexCount];
@@ -182,13 +268,11 @@ public class NusianController {
             Point2D p1 = points[i];
             Point2D p2;
 
-            if (i == vertexCount - 1) {
-                p2 = points[0];
-            } else {
-                p2 = points[i + 1];
-            }
+            if (i == vertexCount - 1) { p2 = points[0]; }
+            else { p2 = points[i + 1]; }
 
             shapeG2D.strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
         }
     }
 }
+>>>>>>>> fa93df4 (i hope i can rebase now):src/main/java/nusextended/m426/NusianController.java
